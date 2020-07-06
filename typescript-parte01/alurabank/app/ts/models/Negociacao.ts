dd8c603bf3c0ddf7629389749ba856f6f07e8019
Celso Ribeiro <celsofilho82@gmail.com>
2020-07-06 09:07:36 -0300

    Iniciando com o TypeScript

---
+++ typescript-parte01/alurabank/app/ts/models/Negociacao.ts
@@ -0,0 +1,31 @@
+class Negociacao {
+    // Criando uma classe que representa o modelo de negociação
+
+    // Definindo as propriedades da classe e os modificadores de acesso
+    private _data;
+    private _quantidade;
+    private _valor;
+
+    constructor(data, quantidade, valor){
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;      
+    }
+
+    get data(){
+        return this._data;
+    }
+
+    get quantidade(){
+        return this._quantidade;
+    }
+
+    get valor(){
+        return this._valor;
+    }
+
+    get volume(){
+        return this._quantidade * this._valor;
+    }       
+
+}
