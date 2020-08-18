7b7aa6a4e4099c81978cf5ad10672eb0ae81f9fc
Celso Ribeiro <celsofilho82@gmail.com>
2020-08-18 09:03:05 -0300

    Melhorando a experiência do usuário

--- angular-parte01/alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ angular-parte01/alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,28 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from "rxjs/operators";
 
-import { PhotoService } from '../photo/photo.service';
 import { Photo } from '../photo/photo';
+import { PhotoListResolver } from './photo-list.resolver';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
+  filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = "";
 
-   // Esse construtor somente é utilizado para injeção de dependência
-   constructor(
-     private photoService: PhotoService,
-     private activatedRoute: ActivatedRoute
-     ){}
+  // Esse construtor somente é utilizado para injeção de dependência
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
+  ) { }
 
-   ngOnInit(): void{
-     const username = this.activatedRoute.snapshot.params.userName;
-     this.photoService
-     .listFromUser(username)
-     .subscribe(photos => this.photos = photos)
-   }
+  ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
+    this.photos = this.activatedRoute.snapshot.data['photos'];
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
+
+  load() {
+    this.photoService
+      .listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe(photos => {
+        this.photos = this.photos.concat(photos);
+        if (!photos.length) this.hasMore = false;
+      });
+  }
 }
--- angular-parte01/alurapic/src/app/photos/photo/photo.service.ts
+++ angular-parte01/alurapic/src/app/photos/photo/photo.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Photo } from "./photo";
 
@@ -7,12 +7,19 @@ const API = 'http://localhost:3000';
 
 // Com essa anotação informamos que nossa classe pode ser injetável 
 // e vamos utilizar somente uma instância para toda a aplicação
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class PhotoService {
 
-    constructor(private http: HttpClient){}
+    constructor(private http: HttpClient) { }
 
-    listFromUser(userName: string){
-       return this.http.get<Photo[]>(API + `/${userName}/photos`);     
+    listFromUser(userName: string) {
+        return this.http.get<Photo[]>(API + `/${userName}/photos`);
+    }
+
+    listFromUserPaginated(userName: string, page: number) {
+
+        const params = new HttpParams().append('page', page.toString());
+
+        return this.http.get<Photo[]>(API + `/${userName}/photos`, { params });
     }
 }
